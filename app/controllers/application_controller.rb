d9dfd97ad31932d5a36fe167fe6c6e5674a5fdfa
Damir Salakhov <damirsalakhov@gmail.com>
2025-12-13 14:53:06 +0700

    Add Current object and prepare views structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include Pagy::Backend
 
+  before_action :set_current_user
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+
+  def set_current_user
+    Current.user = current_user if user_signed_in?
+  end
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
