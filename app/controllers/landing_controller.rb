9c784c071c8d67e573fef000ebee47b0f387ab01
Damir Salakhov <damirsalakhov@gmail.com>
2025-12-13 18:00:59 +0700

    Add Remember me checkbox and testing documentation
    - Add Remember me checkbox to sign in form
    - Add devise_mapping helper to landing controller
    - Update User.md to mark Remember me as implemented
    - Create Testing.md with smoke tests checklist

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,6 +2,12 @@ class LandingController < ApplicationController
   def index
     @resource = User.new
     @resource_name = :user
+    @devise_mapping = Devise.mappings[:user]
   end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  helper_method :devise_mapping
 end
 
