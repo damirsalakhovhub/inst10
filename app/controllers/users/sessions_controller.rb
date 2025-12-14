7f48971ec5c11f4e06d70b1a3ae13afb5cd57f88
Damir Salakhov <damirsalakhov@gmail.com>
2025-12-14 15:51:54 +0700

    Fix login error message translation
    - Use I18n.t with explicit devise.failure.invalid key
    - Replace set_flash_message! which was looking in wrong scope
    - Show 'Invalid Email or password' instead of translation missing

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@ class Users::SessionsController < Devise::SessionsController
     super do |resource|
       # If sign in failed, redirect to landing page with error message
       unless resource.persisted?
-        set_flash_message!(:alert, :invalid)
+        flash[:alert] = I18n.t("devise.failure.invalid", authentication_keys: "Email")
         redirect_to root_path and return
       end
     end
@@ -21,7 +21,7 @@ class Users::SessionsController < Devise::SessionsController
     if resource.persisted?
       super
     else
-      set_flash_message!(:alert, :invalid)
+      flash[:alert] = I18n.t("devise.failure.invalid", authentication_keys: "Email")
       redirect_to root_path
     end
   end
