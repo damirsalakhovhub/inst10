d5eda39f5fe767e0d37ee0339e46567895af17c5
Damir Salakhov <damirsalakhov@gmail.com>
2025-12-14 12:54:39 +0700

    Simplify dev setup: use bin/dev, port 5000, disable component preloading

--- config/initializers/view_component.rb
+++ config/initializers/view_component.rb
@@ -1,10 +1,3 @@
-# Preload UI kit components in development to avoid autoload issues in views
-if Rails.env.development?
-  Rails.application.config.to_prepare do
-    # Force load UI kit components
-    Dir[Rails.root.join("ui_kit/components/**/*_component.rb")].each do |file|
-      require_dependency file
-    end
-  end
-end
+# UI kit components are autoloaded via config/application.rb
+# No preloading in development to allow hot reloading
 
