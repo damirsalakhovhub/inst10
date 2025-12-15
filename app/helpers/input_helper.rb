d50b107562ff9ddb0c66b841825792f92f1f1603
Damir Salakhov <damirsalakhov@gmail.com>
2025-12-15 19:06:45 +0500

    Migrate Input from ViewComponent to classic Rails approach and remove all ViewComponents
    - Created app/helpers/input_helper.rb (37signals style)
    - Created app/views/shared/_input.html.erb partial
    - Created app/assets/stylesheets/components/input.scss (imports from ui_kit via load-path)
    - Replaced all Input::InputComponent usage with render "shared/input"
    - Deleted all ViewComponent files:
      - ui_kit/components/input/input_component.rb
      - ui_kit/components/input/input_component.html.erb
      - ui_kit/components/input/input_component.scss
      - ui_kit/components/button/button_component.rb
      - ui_kit/components/button/button_component.html.erb
    - All inputs now use classic Rails partial approach
    - Tests pass
    - ViewComponent completely removed from project

---
+++ app/helpers/input_helper.rb
@@ -0,0 +1,51 @@
+module InputHelper
+  # Generate ID from name attribute (converts "user[email]" to "user_email")
+  def input_id_from_name(name)
+    name.to_s.gsub(/\[|\]/, '_').gsub(/__/, '_').gsub(/_$/, '')
+  end
+
+  # Auto-detect autocomplete value based on type and name
+  def auto_detect_autocomplete(type:, name:)
+    # Password fields
+    return "current-password" if type == "password"
+    
+    # Email fields
+    return "email" if type == "email" || name.to_s.match?(/email|mail/i)
+    
+    # Username fields
+    return "username" if name.to_s.match?(/username|user_name|user-name|login/i)
+    
+    # Name fields
+    return "name" if name.to_s.match?(/^name$|^full_name$|full-name/i)
+    return "given-name" if name.to_s.match?(/first_name|first-name|given_name|given-name|fname/i)
+    return "family-name" if name.to_s.match?(/last_name|last-name|family_name|family-name|lname|surname/i)
+    
+    # Phone fields
+    return "tel" if type == "tel" || name.to_s.match?(/phone|tel|mobile/i)
+    
+    # Other common fields
+    return "organization" if name.to_s.match?(/company|organization|org/i)
+    return "address-line1" if name.to_s.match?(/address|street/i)
+    return "country" if name.to_s.match?(/country/i)
+    return "postal-code" if name.to_s.match?(/zip|postal|postcode/i)
+    
+    nil
+  end
+
+  # Render icon SVG by loading from ui_kit/assets/images/icons/ directory
+  def render_input_icon(icon_name)
+    icon_path = Rails.root.join("ui_kit/assets/images/icons/#{icon_name}-icon.svg")
+    
+    if File.exist?(icon_path)
+      svg_content = File.read(icon_path)
+      # Remove width/height attributes to allow scaling
+      svg_content = svg_content.gsub(/\s(width|height)=["'][^"']*["']/, '')
+      # Ensure SVG inherits size and color from parent
+      svg_content = svg_content.gsub(/<svg/, '<svg style="width: 100%; height: 100%;" class="input-icon-svg"')
+      svg_content.html_safe
+    else
+      nil
+    end
+  end
+end
+
